74e0da29c460887b7d789aac384f18008d2d0cd1
Luke Graham <luke.evan.graham@gmail.com>
2023-12-15 11:23:27 -0800

    adding server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const db = require("./models")
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening on port %s", PORT)
+  })
+})
