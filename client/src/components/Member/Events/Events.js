6e7c7ad00c1eed1e28a11604c67b24e98b78e12a
Luke Graham <luke.evan.graham@gmail.com>
2024-01-08 17:29:46 -0800

    adding a data table to Events

--- client/src/components/Member/Events/Events.js
+++ client/src/components/Member/Events/Events.js
@@ -4,21 +4,67 @@ import Button from "../../UI/Button/Button";
 import server from "../../../apis/server";
 import { useAuth } from "../../../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
 
 const Events = () => {
   const { authUser } = useAuth();
 
   const [eventList, setEventList] = useState(null);
+  const [colDefs, setColDefs] = useState(null);
 
   useEffect(() => {
     const getEvents = async () => {
       const eventListRes = await server.get(`/event/org/${authUser.orgId}`);
 
-      setEventList(eventListRes.data);
+      // console.log("HERE: ", Object.keys(eventListRes.data[0]));
+
+      let setupColDefs = [
+        { field: "name", filter: true },
+        { field: "startDatePretty", headerName: "Start Date" },
+      ];
+
+      let prettyDateEventList = [];
+
+      // const prettyDateEventList = eventListRes.data.map((event) => {
+      //   console.log("E: ", event);
+      //   event.startDatePretty = new Date(event.startDate).toLocaleDateString(
+      //     "en-US"
+      //   );
+      // });
+
+      eventListRes.data.forEach((event) => {
+        prettyDateEventList.push({
+          ...event,
+          startDatePretty: new Date(event.startDate).toLocaleDateString("en-US"),
+        });
+      });
+
+      // Object.keys(eventListRes.data[0]).forEach((col) => {
+      //   setupColDefs.push({ field: col });
+      // });
+
+      console.log("HERE: ", prettyDateEventList);
+
+      setEventList(prettyDateEventList);
+      setColDefs(setupColDefs);
     };
 
     getEvents();
-  }, [setEventList]);
+  }, [setEventList, setColDefs]);
+
+  // if (eventList && eventList.length) {
+  //   let setupColDefs = [];
+
+  //   Object.keys(eventList[0]).forEach((col) => {
+  //     setupColDefs.push({ field: col });
+  //   });
+
+  //   console.log("DEFS: ", setupColDefs);
+
+  //   setColDefs(setupColDefs);
+  // }
 
   return (
     <>
@@ -28,7 +74,8 @@ const Events = () => {
       <br />
       {console.log("EL: ", eventList)}
       <h2>Event list</h2>
-      {eventList
+      <AgGridReact rowData={eventList} columnDefs={colDefs} />
+      {/* {eventList
         ? eventList.map((event) => (
             <div key={event.id} style={{ display: "flex" }}>
               <div>{`${event.name} @ ${new Date(
@@ -43,7 +90,7 @@ const Events = () => {
               })}`}</div>
             </div>
           ))
-        : null}
+        : null} */}
     </>
   );
 };
