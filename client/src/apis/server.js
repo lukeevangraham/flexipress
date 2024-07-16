7084b14895c8a25319dd26cb1cc776037be2a12c
Luke Graham <luke.evan.graham@gmail.com>
2024-07-16 15:36:42 -0700

    working on my proxy

--- client/src/apis/server.js
+++ client/src/apis/server.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/api'
-})
+  baseURL: "http://localhost:3001/api",
+  withCredentials: false,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+});
