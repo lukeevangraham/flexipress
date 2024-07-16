4cd9a6e48ccd904e9b6995093c34cc73cbb20208
Luke Graham <luke.evan.graham@gmail.com>
2024-07-16 15:33:04 -0700

    adding cors to server

--- server.js
+++ server.js
@@ -83,6 +83,20 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 db.sequelize.sync().then(() => {
   app.listen(PORT, function () {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
