a41867002ae78b80f56189d366c1a324d31a2e57
Luke Graham <luke.evan.graham@gmail.com>
2024-09-16 14:16:19 -0700

    save port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const cloudinary = require("cloudinary");
 
 // Enable CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5959");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Methods",
