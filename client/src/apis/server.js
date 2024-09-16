e82615f0644e09e08cc22dd4523bc1d99d633535
Luke Graham <luke.evan.graham@gmail.com>
2024-09-16 14:32:56 -0700

    more server headers

--- client/src/apis/server.js
+++ client/src/apis/server.js
@@ -3,4 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:3001/api",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
